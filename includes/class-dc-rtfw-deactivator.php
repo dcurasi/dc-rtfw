ec6dd2017b518acd9cce821e8acae77faf624f87
Dario Curasi <dariocurasi@libero.it>
2016-09-09 21:16:16 +0200

    add base boilerplate

---
+++ includes/class-dc-rtfw-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/dcurasi
+ * @since      1.0.0
+ *
+ * @package    Dc_Rtfw
+ * @subpackage Dc_Rtfw/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Dc_Rtfw
+ * @subpackage Dc_Rtfw/includes
+ * @author     Dario Curasì <curasi.d87@gmail.com>
+ */
+class Dc_Rtfw_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
